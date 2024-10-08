133fa4870de130cb1576011b24edbf59aa8767a7
siubie <putraprima@gmail.com>
2024-10-09 05:58:52 +0900

    Add StatsOverview widget to display statistics about active users, bounce rate, and average time on page.

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\User;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        //count all active user
+        $activeUser = User::count();
+        return [
+            Stat::make('Active User', $activeUser)
+                ->description('32k increase')
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('success'),
+            Stat::make('Bounce rate', '21%')
+                ->description('7% increase')
+                ->descriptionIcon('heroicon-m-arrow-trending-down')
+                ->color('danger'),
+            Stat::make('Average time on page', '3:12')
+                ->description('3% increase')
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('success'),
+        ];
+    }
+}
